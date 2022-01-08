76e07f02a688b1b7bf26bbaf5b8c7d19740fd865
Javier Llorente <javier@opensuse.org>
2022-01-08 11:50:44 +0100

    Move Weblate and WeblateHttp from wlfx, implement UserAgent

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.javierllorente.jwl {
+    requires java.naming;
+    requires java.base;
+    requires java.logging;
+    requires java.net.http;
+    requires jakarta.json;
+
+    exports com.javierllorente.jwl;
+}
