8518ea8112a061f4e39558f28af39d94ea7bf23f
Javier Llorente <javier@opensuse.org>
2022-03-24 13:34:18 +0100

    Add missing jersey.media.multipart to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.javierllorente.jwl {
     requires jakarta.ws.rs;
     requires jersey.common;
     requires jersey.client;
+    requires jersey.media.multipart;
     requires jakarta.json;
 
     exports com.javierllorente.jwl;
